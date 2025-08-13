c293d58fd579f185bcdbf63a08ef93994a789f37
taimoor-shan <tshanajk@gmail.com>
2025-08-14 01:23:34 +0500

    minor responsive touch

--- platform/themes/homzen/views/real-estate/single-layouts/partials/project.blade.php
+++ platform/themes/homzen/views/real-estate/single-layouts/partials/project.blade.php
@@ -2,7 +2,7 @@
     <div @class(['single-property-project', $class ?? null])>
         <div class="box-project mt-3 justify-content-between gap-4">
             
-            <div class="project-info">
+            <div class="project-info mb-4 mb-lg-0">
                 <div class="h7 title fw-6 mb-4">{{ __("This building is part of the :project project", ['project' => $project->name]) }}</div>
                 <a class="tf-btn secondary sm" href="{{ $project->url }}">
                     {{  __("View the project") }}
